9f09b5d89a67c3773f661883cd24ef2e8b33dda4
Pavel Benacek <pavel.benacek@gmail.com>
2020-12-20 18:30:14 +0100

    Project translation script added.

--- proj/translate_project.tcl
+++ proj/translate_project.tcl
@@ -6,10 +6,17 @@
 #  WEBSITE: https://github.com/benycze/zybo-base
 # -------------------------------------------------------------------------------
 
+# Main body ###################################################################
 if {[info exists ::xsa_file]} {
 	set xsa_file $::xsa_file
 } else {
-	set xsa_file [concat [file normalize [file dirname [info script]]] "/board_design_wrapper.xsa"]
+	set xsa_file [join [file normalize [file dirname [info script]]] "/board_design_wrapper.xsa"]
+}
+
+if {[info exists ::results_dir]} {
+	set results_dir $::results_dir
+} else {
+	set results_dir [join [file normalize [file dirname [info script]]] "/results"]
 }
 
 # Run the translation flow
@@ -19,13 +26,28 @@ open_project zybo-base.xpr
 
 # Run the synthesis, implementation, write a bitstream and export the architecture
 puts "INFO: Translating the desing ..."
-launch_runs synth_1 -jobs 2 -verbose
-launch_runs impl_1 -jobs 2 -verbose
-launch_runs impl_1 -to_step write_bitstream -jobs 2
-
+    # Synthesis ---------------------------------
+synth_design
+write_checkpoint -force ${results_dir}/post_synth.dcp
+    # Optimizations & routing -------------------
+opt_design
+route_design
+write_checkpoint -force ${results_dir}/post_route.dcp
+    # Placing -----------------------------------
+place_design
+phys_opt_design
+write_checkpoint -force ${results_dir}/post_place.dcp
+    # Reporting ---------------------------------
+report_route_status -file ${results_dir}/post_route_status.rpt
+report_timing_summary -file ${results_dir}/post_route_timing_summary.rpt
+report_utilization -file ${results_dir}/post_place_util.rpt
+report_power -file ${results_dir}/post_route_power.rpt
+report_drc -file ${results_dir}/post_imp_drc.rpt
+report_io -file ${results_dir}/post_imp_io.rpt
+    # Bitstream generation ----------------------
+write_bitstream -verbose -force ${results_dir}/top.bit
+    # XSA export --------------------------------
 puts "INFO: Exporting the design to XSA: $xsa_file"
 write_hw_platform -fixed -force  -include_bit -file $xsa_file
 
-puts "***************************"
-puts "DONE!"
-puts "***************************"
+puts "INFO: DONE!"
