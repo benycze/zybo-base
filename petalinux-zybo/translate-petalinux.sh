65fb7c82ee498ee9928b2be27729d7faa2183611
Pavel Benacek <pavel.benacek@gmail.com>
2020-12-23 09:33:51 +0100

    First version of helping petalinux translation script was created.

--- petalinux-zybo/translate-petalinux.sh
+++ petalinux-zybo/translate-petalinux.sh
@@ -1,6 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
+# Functions & code --------------------------------------------------------------
+
+function print_usage {
+    echo "The script has following commands:"
+    echo "-k         => run the kernel configuration"
+    echo "-r         => run the rootfs configuration"
+    echo "-h         => prints the HELP"
+    echo ""
+    echo "The interactive mode is started if you don't pass any argument."
+}
+
+# Arguments parsing -----------------------------------------------
+# Parse arguments ----------------------------------------------------------------
+kernel_config=0
+rootfs_config=0
+
+while getopts "krh" opt; do
+    case "$opt" in
+    h)
+        print_usage
+        exit 0
+        ;;
+    k)  kernel_config=1
+        ;;
+    r)  rootfs_config=1
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Main code -------------------------------------------------------
+
 echo "#####################################################"
 echo "Importing XSA file ..."
 echo "#####################################################"
@@ -17,11 +49,28 @@ petalinux-config --get-hw-description ${XSA_FILE}
 echo "#####################################################"
 echo "Running the rootfs configuration..."
 echo "#####################################################"
-petalinux-config -c rootfs
+if [ ${rootfs_config} -eq 1 ]; then
+    petalinux-config -c rootfs
+fi
+
+echo "#####################################################"
+echo "Running the kernel configuration ..."
+echo "#####################################################"
+if [ ${kernel_config} -eq 1 ]; then
+    petalinux-config -c kernel
+fi
 
 echo "#####################################################"
 echo "Running the translation ..."
 echo "#####################################################"
 petalinux-build
 
-#TODO: Impelement packing
+echo "#####################################################"
+echo "Running the packaging ..."
+echo "#####################################################"
+petalinux-package --boot --force --fsbl images/linux/zynq_fsbl.elf --fpga images/linux/system.bit --u-boot
+
+echo "#####################################################"
+echo "DONE! We are ready to boot :-)"
+echo "#####################################################"
+exit 0
